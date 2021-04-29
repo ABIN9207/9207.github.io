463f1ea9f0aa037941d8c6c9f2f3d52ffacf0ce7
ABIN9207 <83400109+ABIN9207@users.noreply.github.com>
2021-04-29 20:16:13 +0530

    Add files via upload

---
+++ precache-manifest.4301b354f500bacddb4a3c4b145b4248.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94b9f593516a38b670a5",
+    "url": "/static/css/main.c703114a.chunk.css"
+  },
+  {
+    "revision": "94b9f593516a38b670a5",
+    "url": "/static/js/main.94b9f593.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a0fdf0c11bbad22b089de3d195f0af4d",
+    "url": "/index.html"
+  }
+];
